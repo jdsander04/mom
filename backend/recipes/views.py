be5f6be67c892b5115f3af164e334c1086c1964a
Joel Sander <jdsander04@gmail.com>
2025-09-25 10:26:38 -0400

    My recipe endpoint

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def recipe_list(request):
+    recipe_ids = [1, 2, 3, 4, 5]
+    return JsonResponse({'recipe_ids': recipe_ids})
