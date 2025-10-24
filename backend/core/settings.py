94f71858cb724a823109869b6071b5938d116e43
Joel Sander <jdsander04@gmail.com>
2025-10-24 10:23:49 -0400

    settings fix

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -200,7 +200,9 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000', 
     'http://127.0.0.1:3000',
     'http://localhost:5173',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'http://localhost:8081',
+    'http://127.0.0.1:8081'
 ]
 
 
